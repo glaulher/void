bc6adb7c0d5dc287a950a066add4fd748742015e
glaulher <53663328+glaulher@users.noreply.github.com>
2022-06-29 11:15:02 -0300

    Update installe17.sh

--- installe17.sh
+++ installe17.sh
@@ -2,44 +2,44 @@
 
 
 # REPOSITORIES
-xbps-install -Suv void-repo-multilib void-repo-nonfree
+xbps-install -Svy void-repo-multilib void-repo-nonfree
 
 # XORG PACKAGES
-xbps-install -Suv xorg-minimal xorg-input-drivers xorg-video-drivers xrandr setxkbmap xauth vdpauinfo libvdpau-va-gl font-misc-misc terminus-font dejavu-fonts-ttf dbus-elogind-x11
+xbps-install -Svy xorg-minimal xorg-input-drivers xorg-video-drivers xrandr setxkbmap xauth vdpauinfo libvdpau-va-gl font-misc-misc terminus-font dejavu-fonts-ttf dbus-elogind-x11
   
 # KERNEL
-xbps-install -Suv linux-firmware-network dkms linux-firmware
+xbps-install -Svy linux-firmware-network dkms linux-firmware
 
 # SOUND
-xbps-install -Suv alsa-plugins-pulseaudio paprefs pavucontrol pipewire alsa-utils gstreamer1-pipewire
+xbps-install -Svy alsa-plugins-pulseaudio paprefs pavucontrol pipewire alsa-utils gstreamer1-pipewire
 
 # NETWORK
-xbps-install -Suv  NetworkManager network-manager-applet ntp wget
+xbps-install -Svy  NetworkManager network-manager-applet ntp wget
 
 # DESKTOP ENVIRONMENT
-xbps-install -Suv lightdm lightdm-gtk3-greeter enlightenment xdg-utils xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-kde xdg-user-dirs xdg-user-dirs-gtk libappindicator AppStream
+xbps-install -Svy lightdm lightdm-gtk3-greeter enlightenment xdg-utils xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-kde xdg-user-dirs xdg-user-dirs-gtk libappindicator AppStream
 
 # LOOK AND FEEL
-xbps-install -Suv adwaita-icon-theme Adapta gnome-themes-standard
+xbps-install -Svy adwaita-icon-theme Adapta gnome-themes-standard
 
 # FILE SYSTEM
-xbps-install -Suv ntfs-3g exfat-utils fuse-exfat gvfs-afc gvfs-mtp udisks2
+xbps-install -Svy ntfs-3g exfat-utils fuse-exfat gvfs-afc gvfs-mtp udisks2
 
 # AUTH
-xbps-install -Suv elogind dbus-elogind 
+xbps-install -Svy elogind dbus-elogind 
 
 # DEVELOPMENT
-xbps-install -Suv git mousepad vscode
+xbps-install -Svy git mousepad vscode
 
 # ARCHIVE SOFTWARE
-xbps-install -Suv dtrx p7zip
+xbps-install -Svy dtrx p7zip
 
 # POWER MANAGEMENT
-xbps-install -Suv  upower
+xbps-install -Svy  upower
 
 # UTILITIES
-xbps-install -Suv htop bash-completion unzip
+xbps-install -Svy htop bash-completion unzip
 
 # INTERNET
-xbps-install -Suv firefox
+xbps-install -Svy firefox
 
