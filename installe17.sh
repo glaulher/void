86c2c1b00ba988b8fbc3eb908d997f84d6bb3377
glaulher <53663328+glaulher@users.noreply.github.com>
2022-06-30 18:02:27 -0300

    Update installe17.sh

--- installe17.sh
+++ installe17.sh
@@ -16,19 +16,19 @@ xbps-install -Svy alsa-plugins-pulseaudio paprefs pavucontrol pipewire alsa-util
 xbps-install -Svy  NetworkManager network-manager-applet ntp wget
 
 # DESKTOP ENVIRONMENT
-xbps-install -Svy lightdm lightdm-gtk3-greeter enlightenment terminology xdg-utils xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-kde xdg-user-dirs xdg-user-dirs-gtk libappindicator AppStream
+xbps-install -Svy lightdm lightdm-gtk3-greeter enlightenment terminology qt5ct xdg-utils xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-kde xdg-user-dirs xdg-user-dirs-gtk libappindicator AppStream
 
 # LOOK AND FEEL
 xbps-install -Svy adwaita-icon-theme Adapta gnome-themes-standard
 
 # FILE SYSTEM
-xbps-install -Svy ntfs-3g exfat-utils fuse-exfat gvfs-afc gvfs-mtp udisks2
+xbps-install -Svy ntfs-3g exfat-utils fuse-exfat gvfs-afc gvfs-mtp udisks2 pcmanfm-qt
 
 # AUTH
 xbps-install -Svy elogind dbus-elogind 
 
 # DEVELOPMENT
-xbps-install -Svy git mousepad vscode nano
+xbps-install -Svy git FeatherPad vscode nano
 
 # ARCHIVE SOFTWARE
 xbps-install -Svy dtrx p7zip
